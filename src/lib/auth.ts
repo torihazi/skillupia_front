1d595ac938cca2fcec60c8cb9d293d46f75cdd7d
torihazi <lovebarusa19981225@icloud.com>
2025-11-30 17:41:57 +0900

    fix auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,4 +4,31 @@ import Google from "next-auth/providers/google";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   debug: true,
   providers: [Google],
+  callbacks: {
+    // override default jwt callback
+    jwt: async ({ token, account }) => {
+      if (account) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          expiresAt: account.expires_at,
+          refreshToken: account.refresh_token,
+        };
+      }
+
+      // TODO: token rotationは後々実装する
+
+      return token;
+    },
+
+    session: async ({ session, token }) => {
+      return {
+        ...session,
+        // このaccess_tokenを使って rails のapiに投げる
+        accessToken: token.accessToken,
+        // refresh token は 露出させる必要がないので、ここでは設定しない
+        // rails apiに投げるまでに tokenがexpireしたら401が帰って、落ちるからそれは良い。
+      };
+    },
+  },
 });
